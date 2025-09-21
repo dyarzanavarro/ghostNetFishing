fc169032fffda534abfed46ba79fdfe949a34e0c
Dan <danielyarza1@gmail.com>
2025-09-21 11:52:52 +0200

    Initialized Spring

---
+++ src/main/java/ghostnetfishing/demo/GhostnetfishingIpwa0201Application.java
@@ -0,0 +1,13 @@
+package ghostnetfishing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostnetfishingIpwa0201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostnetfishingIpwa0201Application.class, args);
+	}
+
+}
---
+++ src/test/java/ghostnetfishing/demo/GhostnetfishingIpwa0201ApplicationTests.java
@@ -0,0 +1,13 @@
+package ghostnetfishing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostnetfishingIpwa0201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
